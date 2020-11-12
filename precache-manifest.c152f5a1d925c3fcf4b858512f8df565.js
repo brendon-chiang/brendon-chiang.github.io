f094c56d04127984c8eea675b48a81e304f2c2f1
Brendon Chiang <brendon.chiang@gmail.com>
2020-11-11 17:50:43 -0800

    Updates

---
+++ precache-manifest.c152f5a1d925c3fcf4b858512f8df565.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ec877dcf726834072cbefda1dd7e001",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5e937e513bc1b9075452",
+    "url": "/static/css/2.99bdb016.chunk.css"
+  },
+  {
+    "revision": "6a024f4a212b32220c91",
+    "url": "/static/css/main.ad03a395.chunk.css"
+  },
+  {
+    "revision": "5e937e513bc1b9075452",
+    "url": "/static/js/2.540e0aff.chunk.js"
+  },
+  {
+    "revision": "ecc97ff064b454ab41177529471312c0",
+    "url": "/static/js/2.540e0aff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6a024f4a212b32220c91",
+    "url": "/static/js/main.09862e81.chunk.js"
+  },
+  {
+    "revision": "ea417694d56b324be917",
+    "url": "/static/js/runtime-main.2e20bb00.js"
+  },
+  {
+    "revision": "70f40b0f362095d9544ebbd3c8293bf5",
+    "url": "/static/media/best-app-development-tool.70f40b0f.md"
+  },
+  {
+    "revision": "bdea94e63d3da5dee4bbf391f6a0a24e",
+    "url": "/static/media/common-misconceptions-about-payment.bdea94e6.md"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "78d248241b422acf5d590bcdda06139c",
+    "url": "/static/media/things-to-know-about-startup-business.78d24824.md"
+  }
+]);
